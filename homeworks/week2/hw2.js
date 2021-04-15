ea1934dfc00403255d52342bb587596861a047d8
oack7426 <oack7426@gmail.com>
2021-04-15 22:36:28 +0800

    week2 hw1跟hw2作業完成

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,14 @@
+// a-z
+function checkEng(num){
+    var reg= /^[a-z]+$/;
+    return reg.test(num);
+}
+
 function capitalize(str) {
-  
+  if(checkEng(str[0])){
+    str[0].toUpperCase();
+  }
+  return str;
 }
 
 console.log(capitalize('hello'));
