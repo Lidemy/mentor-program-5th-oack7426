2fce605556ec6f032ce09aa89c6a062b7d03ecb8
oack7426 <oack7426@gmail.com>
2021-04-25 00:15:37 +0800

    hw5

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -1,3 +1,5 @@
+// 沒考慮到正負數
+
 const readline = require('readline')
 
 const rl = readline.createInterface({
@@ -11,30 +13,71 @@ rl.on('line', (line) => {
 })
 
 function solve(lines) {
-  for (let i = 0; i < lines.length; i++) {
+  for (let i = 1; i < lines.length; i++) {
     let arr = []
     arr = lines[i].split(' ')
-    const NumA = Number(arr[0])
-    const NumB = Number(arr[1])
-    const mathNum = Number(arr[2])
-    if (arr.length === 3) {
-      if (mathNum === 1) {
-        if (NumA - NumB > 0) {
-          console.log('A')
-        } else if (NumA - NumB === 0) {
-          console.log('DRAW')
-        } else {
-          console.log('B')
-        }
+    if (Number(arr[2]) === 1) {
+      if (arr[0][0] === '-' && arr[1][0] !== '-') {
+        console.log('B')
+        continue
       }
-      if (mathNum === -1) {
-        if (NumA - NumB > 0) {
-          console.log('B')
-        } else if (NumA - NumB === 0) {
+      if (arr[0][0] !== '-' && arr[1][0] === '-') {
+        console.log('A')
+        continue
+      }
+      if (arr[0].length === arr[1].length) {
+        if (arr[0] === arr[1]) {
           console.log('DRAW')
-        } else {
-          console.log('A')
+          continue
         }
+        for (let j = 0; j < arr[0].length; j++) {
+          if (arr[0][j] > arr[1][j]) {
+            console.log('A')
+            break
+          }
+          if (arr[0][j] < arr[1][j]) {
+            console.log('B')
+            break
+          }
+        }
+      } else if (arr[0].length > arr[1].length) {
+        console.log('A')
+        continue
+      } else {
+        console.log('B')
+        continue
+      }
+    }
+    if (Number(arr[2]) === -1) {
+      if (arr[0][0] === '-' && arr[1][0] !== '-') {
+        console.log('A')
+        continue
+      }
+      if (arr[0][0] !== '-' && arr[1][0] === '-') {
+        console.log('B')
+        continue
+      }
+      if (arr[0].length === arr[1].length) {
+        if (arr[0] === arr[1]) {
+          console.log('DRAW')
+          continue
+        }
+        for (let k = 0; k < arr[0].length; k++) {
+          if (arr[0][k] > arr[1][k]) {
+            console.log('B')
+            break
+          }
+          if (arr[0][k] < arr[1][k]) {
+            console.log('A')
+            break
+          }
+        }
+      } else if (arr[0].length > arr[1].length) {
+        console.log('B')
+        continue
+      } else {
+        console.log('A')
+        continue
       }
     }
   }
