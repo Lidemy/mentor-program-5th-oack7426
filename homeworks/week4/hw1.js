95217d4e45b2242128bcb27be72d0db197c618a1
oack7426 <oack7426@gmail.com>
2021-04-30 20:00:41 +0800

    hw1-修正

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -3,7 +3,7 @@ const request = require('request')
 function getAPI(error, response, body) {
   const info = JSON.parse(body)
   for (let i = 0; i < 10; i++) {
-    console.log(info[i].name)
+    console.log(info[i].id + info[i].name)
   }
 }
 
