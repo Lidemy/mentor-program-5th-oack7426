1bccd2ec22de85e052c5d54fa9a36d3ed60e98b6
oack7426 <oack7426@gmail.com>
2021-05-08 20:45:16 +0800

    hw4

---
+++ homeworks/week4/hw4.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+function TopGames() {
+  request({
+    url: 'https://api.twitch.tv/kraken/games/top',
+    method: 'GET',
+    headers: {
+      'Client-ID': 'h03ykqwbe67bo8bvvl6wegc3ampete',
+      'Accept': 'application/vnd.twitchtv.v5+json'   // eslint-disable-line
+    }
+  }, (error, response, body) => {
+    const json = JSON.parse(body)
+    const jsonTop = json.top
+    for (let i = 0; i < jsonTop.length; i++) {
+      console.log(`${jsonTop[i].viewers} ${jsonTop[i].game.name}`)
+    }
+  })
+}
+TopGames()
