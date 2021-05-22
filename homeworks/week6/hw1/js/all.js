de7d60ee44c6c8a62a6b80556f07278d3a8c9944
oack7426 <oack7426@gmail.com>
2021-05-22 21:44:48 +0800

    先忽略共用檔問題

--- homeworks/week6/hw1/js/all.js
+++ homeworks/week6/hw1/js/all.js
@@ -1,4 +1,7 @@
-import $ from 'jquery'
-
-$('#header').load('./component/header.html')
-$('#footer').load('./component/footer.html')
+/* eslint-disable */
+// 暫時不檢查，後面裝 webpack 打包後再修正
+$(function(){
+  $('#header').load('./component/header.html')
+  $('#footer').load('./component/footer.html')
+})
+/* eslint-enable */
