0687983f376a57df712223e80b296292f1ab78f3
oack7426 <oack7426@gmail.com>
2021-06-03 22:47:01 +0800

    hw1 暴力解

--- homeworks/week7/hw1/js/all.js
+++ homeworks/week7/hw1/js/all.js
@@ -1,14 +1,56 @@
-const input = document.querySelectorAll('input[type="text"]')
-const submitBtn = document.getElementById('submit')
+// 暱稱是否為空
+// 電子郵件是否為空
+// 手機號碼是否為空
+// 報名類型是否有勾選
+// 怎麼知道這個活動的是否為空
+// 如果沒有通過檢查，請在欄位的 input 下方顯示紅字提醒，有通過的話跳出一個 alert 展示使用者填寫的資料。 （ＯＫ）
+
+const $input = document.querySelectorAll('.hasValue')
+const $title = document.querySelectorAll('h3')
+const $radio = document.querySelectorAll('input[type="radio"]')
+const $error = document.querySelectorAll('.error')
+const $from = document.querySelector('form')
 
 function submitFn(e) {
+  let fromInfo = ''
+  let inspection = true
+  const allTitle = []
+  const allValue = []
+
   e.preventDefault()
-  for (let i = 0; i < input.length; i++) {
-    console.log(input[i].value)
-    if (input[i].value.length === 0) {
-      console.log(i)
+
+  for (let i = 0; i < $title.length; i++) {
+    allTitle.push($title[i].innerHTML)
+  }
+  for (let i = 0; i < $input.length; i++) {
+    allValue.push($input[i].value)
+  }
+  allValue.splice(3, 0, '')
+  for (let i = 0; i < $radio.length; i++) {
+    if ($radio[i].checked) {
+      const $next = $radio[i].nextElementSibling
+      allValue[3] = $next.innerHTML
+      break
     }
+  }
+
+  for (let i = 0; i < allValue.length; i++) {
+    if (!$error[i]) {
+      break
+    }
+    if (allValue[i].length === 0) {
+      $error[i].style.display = 'block'
+      inspection = false
+    } else {
+      $error[i].style.display = 'none'
+    }
+  }
+  if (inspection) {
+    for (let i = 0; i < $title.length; i++) {
+      fromInfo += `${allTitle[i]} : ${allValue[i]} \n`
+    }
+    alert(fromInfo)
   }
 }
 
-submitBtn.addEventListener('click', submitFn)
+$from.addEventListener('submit', submitFn)
