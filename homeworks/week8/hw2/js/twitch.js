a18780925d90f8aeddc2af93ac76e5849256bc31
oack7426 <oack7426@gmail.com>
2021-06-19 15:27:55 +0800

    hw2 把5個遊戲的呼叫改為function

--- homeworks/week8/hw2/js/twitch.js
+++ homeworks/week8/hw2/js/twitch.js
@@ -1,12 +1,17 @@
 // 請串接 Twitch API，顯示出目前最熱門的 5 個遊戲 (OK)
-// 把第一個遊戲塞到版面上，其他的如法泡製
-// 先把 5個遊戲 的ajax都寫好直接顯示在版面上，不要思考點擊後在呼叫的問題
+// 把第一個遊戲塞到版面上，其他的如法泡製 (OK)
+// 把第三個熱門遊戲的串接改為function (OK)
+// 試著把5個熱門遊戲的串接改為function 跑迴圈(OK)
+// 先把 5個遊戲 的ajax都寫好直接顯示在版面上，不要思考點擊後在呼叫的問題 (OK)
 // 把 header 綁 data ， data 內容是 id 板塊名 （存在一個陣列裡），綁 click 事件，點擊後對應的 id 板塊會打開
+
 // 點下去之後可以顯示正在直播這遊戲的前 20 個實況（要剛好 20 個）
 // 可以切換不同的遊戲，顯示不同遊戲的熱門實況。
 
 // 熱門的 5 個遊戲名稱
 const fiveTopGames = []
+// id 們
+const idList = ['firstGames', 'secondGames', 'thirdGames', 'fourthGames', 'fifthGames']
 
 // 熱門的 5 個遊戲名稱 header
 const request = new XMLHttpRequest()
@@ -32,22 +37,23 @@ request.setRequestHeader('client-id', 'h03ykqwbe67bo8bvvl6wegc3ampete')
 request.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json')
 request.send()
 
-// 第一個熱門遊戲
-const GameFirst = new XMLHttpRequest()
-GameFirst.onload = () => {
-  if (GameFirst.status >= 200 && GameFirst.status < 400) {
-    const json = JSON.parse(GameFirst.responseText)
-    const $firstGames = document.getElementById('firstGames')
-    const title = document.createElement('h2')
-    title.innerHTML = `<b>${fiveTopGames[0]}</b> Top 20 popular live streams sorted by current viewers`
-    $firstGames.prepend(title)
-    for (let i = 0; i < json.streams.length; i++) {
-      const $firstGamesBox = document.querySelector('#firstGames .list-wrap')
-      const a = document.createElement('a')
-      a.classList.add('list-box')
-      a.href = json.streams[i].channel.url
-      a.target = '_blank'
-      a.innerHTML = `<img src="${json.streams[i].preview.medium}" alt="${json.streams[i].channel.display_name}">
+// 熱門遊戲 20 個直播
+function ajax(id, gameName) {
+  const Games = new XMLHttpRequest()
+  Games.onload = () => {
+    if (Games.status >= 200 && Games.status < 400) {
+      const json = JSON.parse(Games.responseText)
+      const $gamesId = document.getElementById(id)
+      const title = document.createElement('h2')
+      title.innerHTML = `<b>${gameName}</b> Top 20 popular live streams sorted by current viewers`
+      $gamesId.prepend(title)
+      for (let i = 0; i < json.streams.length; i++) {
+        const $GamesBox = $gamesId.querySelector('.list-wrap')
+        const a = document.createElement('a')
+        a.classList.add('list-box')
+        a.href = json.streams[i].channel.url
+        a.target = '_blank'
+        a.innerHTML = `<img src="${json.streams[i].preview.medium}" alt="${json.streams[i].channel.display_name}">
                     <div class="list-info-box">
                         <img src="${json.streams[i].channel.logo}" alt="${json.streams[i].channel.display_name}">
                         <div class="list-info-inbox">
@@ -55,53 +61,20 @@ GameFirst.onload = () => {
                             <p>${json.streams[i].channel.display_name}</p>
                         </div>
                     </div>`
-      $firstGamesBox.appendChild(a)
+        $GamesBox.appendChild(a)
+      }
+    } else {
+      alert('系統出錯，請重新整理')
     }
-  } else {
-    alert('系統出錯，請重新整理')
-  }
-  GameFirst.onerror = () => {
-    alert('系統出錯，請重新整理')
-  }
-}
-GameFirst.open('GET', `https://api.twitch.tv/kraken/streams/?game=${fiveTopGames[0]}&limit=20`, false)
-GameFirst.setRequestHeader('client-id', 'h03ykqwbe67bo8bvvl6wegc3ampete')
-GameFirst.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json')
-GameFirst.send()
-
-// 第二個熱門遊戲
-const GameSecond = new XMLHttpRequest()
-GameSecond.onload = () => {
-  if (GameSecond.status >= 200 && GameSecond.status < 400) {
-    const json = JSON.parse(GameSecond.responseText)
-    const $secondGames = document.getElementById('secondGames')
-    const title = document.createElement('h2')
-    title.innerHTML = `<b>${fiveTopGames[1]}</b> Top 20 popular live streams sorted by current viewers`
-    $secondGames.prepend(title)
-    for (let i = 0; i < json.streams.length; i++) {
-      const $secondGamesBox = document.querySelector('#secondGames .list-wrap')
-      const a = document.createElement('a')
-      a.classList.add('list-box')
-      a.href = json.streams[i].channel.url
-      a.target = '_blank'
-      a.innerHTML = `<img src="${json.streams[i].preview.medium}" alt="${json.streams[i].channel.display_name}">
-                    <div class="list-info-box">
-                        <img src="${json.streams[i].channel.logo}" alt="${json.streams[i].channel.display_name}">
-                        <div class="list-info-inbox">
-                            <h3>${json.streams[i].channel.status}</h3>
-                            <p>${json.streams[i].channel.display_name}</p>
-                        </div>
-                    </div>`
-      $secondGamesBox.appendChild(a)
+    Games.onerror = () => {
+      alert('系統出錯，請重新整理')
     }
-  } else {
-    alert('系統出錯，請重新整理')
   }
-  GameSecond.onerror = () => {
-    alert('系統出錯，請重新整理')
-  }
+  Games.open('GET', `https://api.twitch.tv/kraken/streams/?game=${gameName}&limit=20`, false)
+  Games.setRequestHeader('client-id', 'h03ykqwbe67bo8bvvl6wegc3ampete')
+  Games.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json')
+  Games.send()
 }
-GameSecond.open('GET', `https://api.twitch.tv/kraken/streams/?game=${fiveTopGames[1]}&limit=20`, false)
-GameSecond.setRequestHeader('client-id', 'h03ykqwbe67bo8bvvl6wegc3ampete')
-GameSecond.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json')
-GameSecond.send()
+for (let i = 0; i < 5; i++) {
+  ajax(idList[i], fiveTopGames[i])
+}
